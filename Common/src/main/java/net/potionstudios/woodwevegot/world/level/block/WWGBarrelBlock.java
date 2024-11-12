670b9e896c7157bb4e4ee7b7099d8bb49263e3fa
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-11-11 21:42:29 -0500

    Copy Properties

--- Common/src/main/java/net/potionstudios/woodwevegot/world/level/block/WWGBarrelBlock.java
+++ Common/src/main/java/net/potionstudios/woodwevegot/world/level/block/WWGBarrelBlock.java
@@ -2,18 +2,16 @@ package net.potionstudios.woodwevegot.world.level.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.BarrelBlock;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.MapColor;
 import net.potionstudios.woodwevegot.world.level.block.entity.WWGBarrelBlockEntity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class WWGBarrelBlock extends BarrelBlock {
 	public WWGBarrelBlock() {
-		super(Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASS).strength(2.5F).sound(SoundType.WOOD).ignitedByLava());
+		super(Properties.copy(Blocks.BARREL));
 	}
 
 	@Override
--- Common/src/main/java/net/potionstudios/woodwevegot/world/level/block/WWGChestBlock.java
+++ Common/src/main/java/net/potionstudios/woodwevegot/world/level/block/WWGChestBlock.java
@@ -1,14 +1,12 @@
 package net.potionstudios.woodwevegot.world.level.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ChestBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.MapColor;
 import net.potionstudios.woodwevegot.world.level.block.entity.WWGBlockEntities;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,7 +17,7 @@ public class WWGChestBlock extends ChestBlock {
 	private final String set;
 
 	protected WWGChestBlock(Supplier<BlockEntityType<? extends ChestBlockEntity>> blockEntityType, String set) {
-		super(Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASS).strength(2.5F).sound(SoundType.WOOD).ignitedByLava(), blockEntityType);
+		super(Properties.copy(Blocks.CHEST), blockEntityType);
 		this.set = set;
 	}
 	protected WWGChestBlock(String set) {
